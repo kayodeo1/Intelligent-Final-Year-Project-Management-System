eb0942c3e2e503794c1692c13c3bd79b5e33e2bb
kayodeo1 <ojokayode566@gmail.com>
2026-03-03 20:47:50 +0100

    crud bug fixed

--- src/main/java/com/kayode/ifypm/model/Constants.java
+++ src/main/java/com/kayode/ifypm/model/Constants.java
@@ -33,26 +33,6 @@ public class Constants {
 
 	static {
 		APP_BASE_NAME = rs.getString("app.base.name");
-		UPLOAD_PATH_DIR = rs.getString("media.upload.dir.path.mac");
-		UPLOAD_PATH_DIR_MAC = rs.getString("media.upload.dir.path.mac");
-		FILE_UPLOAD_PATH = rs.getString("FILE_UPLOAD_PATH");
-		MAIL_NOTIFICATION_SERVICE = rs.getString("notification.email.service");
-		ISAFE_URL = rs.getString("isafe.url");
-		LOG_PATH = rs.getString("isafe.log");
-
-		// Email Configuration
-		EMAIL_FROM = rs.getString("isafe.email.from");
-
-		// Template 1: Course Creation Notification (For Admins)
-		EMAIL_TEMPLATE1_SUBJECT = rs.getString("isafe.email.template1.subject");
-		EMAIL_TEMPLATE1_BODY = rs.getString("isafe.email.template1.body");
-
-		// Template 2: Course Published Notification (For Target Audience)
-		EMAIL_TEMPLATE2_SUBJECT = rs.getString("isafe.email.template2.subject");
-		EMAIL_TEMPLATE2_BODY = rs.getString("isafe.email.template2.body");
-
-		// Template 3: Course Expiration Warning (For Target Audience)
-		EMAIL_TEMPLATE3_SUBJECT = rs.getString("isafe.email.template3.subject");
-		EMAIL_TEMPLATE3_BODY = rs.getString("isafe.email.template3.body");
+		
 	}
 }
--- src/main/java/com/kayode/ifypm/model/ProgressStatus.java
+++ src/main/java/com/kayode/ifypm/model/ProgressStatus.java
@@ -1,5 +1,7 @@
 package com.kayode.ifypm.model;
 
 public enum ProgressStatus {
+	
+	NOT_STARTED,IN_PROGRESS,COMPLETED
 
 }
